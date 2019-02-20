ffb0b2b4fd5f4aaa9fa0f14e210e60f742e815ad
[Ochieng424] <[ochiengderrick424@gmail.com]>
2019-02-20 10:26:50 +0300

    Fixing->Error: A network error (such as timeout, interrupted connection or unreachable host) has occurred.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
-function logIn()
-{
-    const userEmail = document.getElementById('Email').value;
-    const userPassword = document.getElementById('Password').value;
+// var user = firebase.auth().currentUser;
 
-    window.alert(userEmail + userPassword);
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        window.location.href = 'pages/profile.php';
+    } else {
+        // User is signed out.
+        window.location.href = 'index.html';
+    }
+});
+
+function logIn() {
+    let userEmail = document.getElementById('Email').value;
+    let userPassword = document.getElementById('Password').value;
+
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+
+        window.alert("Error: " + errorMessage);
+    });
 }
